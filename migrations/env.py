af45dd436eee48bea6dd13ae7304b32b686b1007
Benito Martin <116911431+benitomartin@users.noreply.github.com>
2025-06-28 18:46:35 +0200

    test dev

--- migrations/env.py
+++ migrations/env.py
@@ -1,6 +1,8 @@
+import os
 from logging.config import fileConfig
 
 from alembic import context
+from loguru import logger
 from sqlalchemy import engine_from_config, pool
 
 from src.models.db_models import Base
@@ -26,19 +28,20 @@ target_metadata = Base.metadata
 # ... etc.
 
 
+def get_url() -> str:
+    user = os.getenv("POSTGRES_USER")
+    password = os.getenv("POSTGRES_PASSWORD")
+    host = os.getenv("POSTGRES_HOST", "localhost")
+    port = os.getenv("POSTGRES_PORT", "5432")
+    db = os.getenv("POSTGRES_DB")
+    url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}"
+    logger.info(f"Generated database URL: {url}")
+    return url
+
+
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
-    url = config.get_main_option("sqlalchemy.url")
+    """Run migrations in 'offline' mode."""
+    url = get_url()  # Use dynamic URL
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -51,12 +54,7 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
+    """Run migrations in 'online' mode."""
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
